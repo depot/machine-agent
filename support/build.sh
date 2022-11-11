02d9f8f98bd55e954d4f921ce2287ad64dd6c267
Jacob Gillespie <jacobwgillespie@gmail.com>
2022-11-11 12:04:35 +0000

    Fix binary name

--- support/build.sh
+++ support/build.sh
@@ -10,8 +10,8 @@ cd dist
 mkdir -p machine-agent_linux_x64/bin
 mkdir -p machine-agent_linux_arm64/bin
 
-cp machine-agent-linux-x64 machine-agent_linux_x64/bin/machine-agent
-cp machine-agent-linux-arm64 machine-agent_linux_arm64/bin/machine-agent
+cp machine-agent-x64 machine-agent_linux_x64/bin/machine-agent
+cp machine-agent-arm64 machine-agent_linux_arm64/bin/machine-agent
 
 mkdir -p artifacts
 tar -czf artifacts/machine-agent_linux_x64.tar.gz -C machine-agent_linux_x64 .
