88adfdc9f3c5461f13a78821ed5d452f530c2be4
Jacob Gillespie <jacobwgillespie@gmail.com>
2022-11-12 19:45:18 +0000

    Use AbortController to handle exits

--- src/tasks/githubActions.ts
+++ src/tasks/githubActions.ts
@@ -9,6 +9,7 @@ export async function startGitHubActions(
   task: RegisterMachineResponse_GitHubActionsTask,
 ) {
   const {machineId, token} = message
+  const metadata = Metadata({Authorization: `Bearer ${token}`})
   const runnerDir = `/home/runner/runners/${task.runnerVersion}`
 
   await execa(
@@ -34,22 +35,27 @@ export async function startGitHubActions(
     },
   )
 
-  let done = false
+  const controller = new AbortController()
+  const signal = controller.signal
+
   async function* pingHealth() {
     while (true) {
-      if (done) return
+      if (signal.aborted) return
       await sleep(1000)
       yield {machineId}
     }
   }
 
-  const runner = execa('./run', [], {cwd: runnerDir, stdio: 'inherit'}).finally(() => {
-    done = true
-  })
-
-  const healthLoop = client.pingMachineHealth(pingHealth(), {
-    metadata: Metadata({Authorization: `Bearer ${token}`}),
-  })
-
-  await Promise.all([runner, healthLoop])
+  try {
+    await Promise.all([
+      execa('./run.sh', [], {cwd: runnerDir, stdio: 'inherit', signal}).finally(() => {
+        controller.abort()
+      }),
+      client.pingMachineHealth(pingHealth(), {metadata}),
+    ])
+  } catch (error) {
+    throw error
+  } finally {
+    controller.abort()
+  }
 }
