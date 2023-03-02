560bd87c99c0ee3022ef82c11164d954d4187e08
Chris Goller <goller@gmail.com>
2023-03-01 20:35:00 -0600

    feat: add utility to get disk megabytes
    Signed-off-by: Chris Goller <goller@gmail.com>

---
+++ src/utils/disk.ts
@@ -0,0 +1,10 @@
+import {execa} from 'execa'
+
+export async function availableMegabytes(path: string) {
+  const {exitCode, stdout} = await execa('stat', ['-f', '-c', '%a %S', path])
+  if (exitCode == 0) {
+    const output = stdout as string
+    const [freeBlocks, blockSize] = output.split(' ')
+    return Number((BigInt(freeBlocks) * BigInt(blockSize)) / BigInt(1024) / BigInt(1024))
+  }
+}
