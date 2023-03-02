a39ef24e9c7ed1bf16379b9dda3fde34ef9fa87b
Chris Goller <goller@gmail.com>
2023-03-01 20:53:02 -0600

    refactor: add exception handling
    Signed-off-by: Chris Goller <goller@gmail.com>

--- src/utils/disk.ts
+++ src/utils/disk.ts
@@ -1,10 +1,14 @@
 import {execa} from 'execa'
 
 export async function availableMegabytes(path: string) {
-  const {exitCode, stdout} = await execa('stat', ['-f', '-c', '%a %S', path])
-  if (exitCode == 0) {
-    const output = stdout as string
-    const [freeBlocks, blockSize] = output.split(' ')
-    return Number((BigInt(freeBlocks) * BigInt(blockSize)) / BigInt(1024) / BigInt(1024))
+  try {
+    const {exitCode, stdout} = await execa('stat', ['-f', '-c', '%a %S', path])
+    if (exitCode == 0) {
+      const output = stdout as string
+      const [freeBlocks, blockSize] = output.split(' ')
+      return Number((BigInt(freeBlocks) * BigInt(blockSize)) / BigInt(1024) / BigInt(1024))
+    }
+  } catch (e) {
+    console.log(e)
   }
 }
