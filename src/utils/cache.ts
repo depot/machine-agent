f0721be0a87225c46cfb23fcc32d1ddf7b0eeed9
Jacob Gillespie <jacobwgillespie@gmail.com>
2023-07-26 11:54:49 +0100

    Fix disk usage command

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,8 +1,10 @@
 import {execa} from 'execa'
 
 export async function du(): Promise<string | undefined> {
-  const {exitCode, stdout} = await execa('/usr/bin/buildctl', ['du', '--verbose', `--format '{{json .}}'`], {
-    stdio: 'inherit',
+  const {exitCode, stdout} = await execa('/usr/bin/buildctl', ['du', '--verbose', '--format', '{{json .}}'], {
+    stdout: 'pipe',
+    stderr: 'inherit',
+    stdin: 'inherit',
   })
   return exitCode == 0 ? stdout : undefined
 }
