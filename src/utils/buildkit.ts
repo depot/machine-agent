78f01a4319224262021eeb40f2bc844df366bb90
Jacob Gillespie <jacobwgillespie@gmail.com>
2023-08-25 16:16:53 +0100

    Use TLS for `buildctl du`

--- src/utils/buildkit.ts
+++ src/utils/buildkit.ts
@@ -1,10 +1,31 @@
 import {execa} from 'execa'
 
 export async function du(): Promise<string | undefined> {
-  const {exitCode, stdout} = await execa('/usr/bin/buildctl', ['du', '--verbose', '--format', '{{json .}}'], {
-    stdout: 'pipe',
-    stderr: 'inherit',
-    stdin: 'inherit',
-  })
+  const {exitCode, stdout} = await execa(
+    '/usr/bin/buildctl',
+    [
+      '--tlsservername',
+      'localhost',
+      '--tlscert',
+      '/etc/buildkit/tls.crt',
+      '--tlskey',
+      '/etc/buildkit/tls.key',
+      '--tlscacert',
+      '/etc/buildkit/tlsca.crt',
+      '--timeout',
+      '1',
+      '--addr',
+      'tcp://localhost:443',
+      'du',
+      '--verbose',
+      '--format',
+      '{{json .}}',
+    ],
+    {
+      stdout: 'pipe',
+      stderr: 'inherit',
+      stdin: 'inherit',
+    },
+  )
   return exitCode == 0 ? stdout : undefined
 }
