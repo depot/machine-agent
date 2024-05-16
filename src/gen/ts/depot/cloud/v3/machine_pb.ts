3d02d2166561c81ebf20f810e44e27dc3519effe
Chris Goller <goller@gmail.com>
2024-05-16 15:12:28 -0500

    fix: image_spec should not be optional
    We want to imply this is required particularly after volume_name is deprecated.
    
    Signed-off-by: Chris Goller <goller@gmail.com>

--- src/gen/ts/depot/cloud/v3/machine_pb.ts
+++ src/gen/ts/depot/cloud/v3/machine_pb.ts
@@ -380,9 +380,9 @@ export class RegisterMachineResponse_Mount_CephVolume extends Message<RegisterMa
   /**
    * If the image_spec is set use instead of constructing with the volume_name.
    *
-   * @generated from field: optional string image_spec = 5;
+   * @generated from field: string image_spec = 5;
    */
-  imageSpec?: string
+  imageSpec = ''
 
   constructor(data?: PartialMessage<RegisterMachineResponse_Mount_CephVolume>) {
     super()
@@ -396,7 +396,7 @@ export class RegisterMachineResponse_Mount_CephVolume extends Message<RegisterMa
     {no: 2, name: 'client_name', kind: 'scalar', T: 9 /* ScalarType.STRING */},
     {no: 3, name: 'key', kind: 'scalar', T: 9 /* ScalarType.STRING */},
     {no: 4, name: 'ceph_conf', kind: 'scalar', T: 9 /* ScalarType.STRING */},
-    {no: 5, name: 'image_spec', kind: 'scalar', T: 9 /* ScalarType.STRING */, opt: true},
+    {no: 5, name: 'image_spec', kind: 'scalar', T: 9 /* ScalarType.STRING */},
   ])
 
   static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): RegisterMachineResponse_Mount_CephVolume {
